32cf945aa9eb10d26c9a6339739ee7104b3fffcf
Dmitry Tereshchenko <dima123-99@mail.ru>
2022-06-04 16:32:24 +0300

    Bugfix

---
+++ wsgi_sbn/main.py
@@ -0,0 +1,34 @@
+import os
+import sys
+import sbn
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+from server import WSGIServer
+
+SERVER_ADDRESS = (HOST, PORT) = '', 8888
+
+class Main(sbn.Kernel):
+
+    def __init__(self, *args, **kwargs):
+        if len(sys.argv) < 2:
+            print('>>>> Python (WSGI Main) [ERROR]: Provide a WSGI application object as module:callable', file=open('wsgisbn.log', 'a'))
+            sys.exit()
+        module, application = sys.argv[1].split(':')
+        try:
+            self.module = __import__(module)
+            self.application = getattr(self.module, application)
+        except Exception as e:
+            print('>>>> Python (WSGI Main) [ERROR]: %s' % e, file=open('wsgisbn.log', 'a'))
+        super(Main, self).__init__(*args, **kwargs)
+        print('>>>> Python (WSGI Main) [INFO]: ============= program init! =============', file=open('wsgisbn.log', 'a'))
+
+    def act(self):
+        print('>>>> Python (WSGI Main) [INFO]: ============= program start! =============', file=open('wsgisbn.log', 'a'))
+        httpd = WSGIServer(SERVER_ADDRESS, self.application)
+        httpd.enable_carries_parent()  # carries_parent
+        print('>>>> Python (WSGI Main) [INFO]: Serving HTTP on port %s ...' % PORT, file=open('wsgisbn.log', 'a'))
+        sbn.upstream(self, httpd, target=sbn.Target.Remote)
+
+    def react(self, child: sbn.Kernel):
+        print('>>>> Python (WSGI Main) [INFO]: program finished!', file=open('wsgisbn.log', 'a'))
+        sbn.commit(self, target=sbn.Target.Remote)
--- wsgi_sbn/server.py
+++ wsgi_sbn/server.py
@@ -7,31 +7,31 @@ class WSGIServer(sbn.Kernel):
     address_family = socket.AF_INET
     socket_type = socket.SOCK_STREAM
     request_queue_size = 5
-    
+
     def __init__(self, server_address, application):
         super(WSGIServer, self).__init__()
         self._init_server(server_address)
         self._init_app(application)
-        
-        
+
+
     def _init_server(self, server_address):
         # Create a listening socket
-        self.listen_socket = listen_socket = socket.socket(
+        self.listen_socket = socket.socket(
             self.address_family,
             self.socket_type
         )
         # Allow to reuse the same address
-        listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         # Bind
-        listen_socket.bind(server_address)
+        self.listen_socket.bind(server_address)
         # Activate
-        listen_socket.listen(self.request_queue_size)
+        self.listen_socket.listen(self.request_queue_size)
         # Get server host name and port
         host, port = self.listen_socket.getsockname()[:2]
         self.server_name = socket.getfqdn(host)
         self.server_port = port
         # Return headers set by Web framework/Web application
-        self.headers_set = []    
+        self.headers_set = []
 
     def _init_app(self, application):
         self.application = application
@@ -41,11 +41,18 @@ class WSGIServer(sbn.Kernel):
 
     def _serve_forever(self):
         listen_socket = self.listen_socket
+        print('>>>> Python (WSGI Server) [DEBUG]: Socket - %s' % listen_socket, file=open('wsgisbn.log', 'a'))
         while True:
+            print('>>>> Python (WSGI Server) [DEBUG]: Waiting connection', file=open('wsgisbn.log', 'a'))
+            #sbn.sleep(1000)
             # New client connection
-            client_connection, client_address = listen_socket.accept()
+            try:
+                client_connection, client_address = listen_socket.accept()
+            except Exception as e:
+                print('>>>> Python (WSGI Server) [ERROR]: %s' % e, file=open('wsgisbn.log', 'a'))
             # Handle one request and close the client connection. Then
             # loop over to wait for another client connection
+            print('>>>> Python (WSGI Server) [INFO]: Connection from %s' % client_address, file=open('wsgisbn.log', 'a'))
             self._handle_request(client_connection)
 
     def _handle_request(self, client_connection):
